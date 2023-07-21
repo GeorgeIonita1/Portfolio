da48168009926eb75160d78c810911614882fec5
George <g3orrg301@gmail.com>
2023-07-21 22:01:47 +0300

    Added color variables

--- client/src/components/Experience/Experience.tsx
+++ client/src/components/Experience/Experience.tsx
@@ -3,12 +3,12 @@ import ExperienceCard from './ExperienceCard';
 
 export default function Experience() {
     return (
-        <section className={styles.experience}>
+        <section>
             <div className='title-container'>
                 <h5>What I have done so far</h5>
                 <h2>Work Experience</h2>
             </div>
-            <div className={styles.container}>
+            <div className={styles.experience}>
                 <ExperienceCard />
                 <ExperienceCard />
                 <ExperienceCard />

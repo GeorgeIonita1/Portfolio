ccc418da1a33cf26cb2679093c80cd1b45e92cf1
George <g3orrg301@gmail.com>
2023-07-30 16:21:11 +0300

    Added sidebar menu for small devices

--- client/src/components/NavigationBar/NavigationBar.tsx
+++ client/src/components/NavigationBar/NavigationBar.tsx
@@ -1,8 +1,17 @@
+'use client'
+
 import styles from './NavigationBar.module.scss';
 import logoIcon from '../../../public/icons/logo.svg';
 import Image from 'next/image';
+import { useState } from 'react';
 
 export default function NavigationBar() {
+    const [sidebarIsOpen, setSidebarIsOpen] = useState(false);
+
+    const handleSidebarToggle = () => {
+        setSidebarIsOpen(!sidebarIsOpen);
+    }
+
     return (
         <header className={styles.navigation}>
             <div className={styles.navigationLogo}>
@@ -10,8 +19,12 @@ export default function NavigationBar() {
                     <Image src={logoIcon} alt='Logo' />
                 </div>
             </div>
-            <nav>
+            <div className={styles.navigationHamburger} onClick={handleSidebarToggle}>
+                =
+            </div>
+            <nav className={sidebarIsOpen ? styles.navigationOpen : styles.navigationHidden}>
                 <ul>
+                    <div className={styles.navigationClose} onClick={handleSidebarToggle}>x</div>
                     <li>
                         <a href="">WORK</a>
                     </li>

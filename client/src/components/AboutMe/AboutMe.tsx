39f57b411db3a484a04d7b78260db0bb03776481
George <g3orrg301@gmail.com>
2023-08-16 23:02:24 +0300

    Added intersection observer for aboutMe section

--- client/src/components/AboutMe/AboutMe.tsx
+++ client/src/components/AboutMe/AboutMe.tsx
@@ -1,3 +1,4 @@
+'use client'
 // todo: create components for cards;
 // change icons with 3d models
 
@@ -8,8 +9,25 @@ import codingBrackets from '../../../public/pictures/codingBrackets.png';
 import backendIcon from '../../../public/icons/backend.svg';
 import webIcon from '../../../public/icons/web.svg';
 import awardIcon from '../../../public/icons/award.svg';
+import { useEffect } from 'react';
 
 export default function AboutMe() {
+    useEffect(() => {
+        const observer = new IntersectionObserver(entries => {
+            entries.forEach(entry => {
+                if (entry.isIntersecting) {
+                    entry.target.classList.remove('hidden');
+                    entry.target.classList.add('show');
+                }
+            })
+        },
+        { root: null, threshold: 0.25 }
+        );
+
+        const hiddenElements = document.querySelectorAll('.hidden');
+        hiddenElements.forEach(el => observer.observe(el));
+    }, [])
+
     return (
         <section id='about-me' className={styles.aboutMe}>
             <div className='title-container'>
@@ -22,17 +40,17 @@ export default function AboutMe() {
                 </div>
                 <div className={styles.aboutMeDetails}>
                     <div className={styles.aboutMeCards}>
-                        <div className={styles.aboutMeCard}>
+                        <div className={`${styles.aboutMeCard} hidden`}>
                             <div className={`${styles.aboutMeCardImage} svg-container`}><Image src={awardIcon} alt='Award icon' /></div>
                             <h3>Experience</h3>
                             <h4>2+ years working</h4>
                         </div>
-                        <div className={styles.aboutMeCard}>
+                        <div className={`${styles.aboutMeCard} hidden`}>
                             <div className={`${styles.aboutMeCardImage} svg-container`}><Image src={webIcon} alt='Web icon' /></div>
                             <h3>Web development</h3>
                             <h4>3 years</h4>
                         </div>
-                        <div className={styles.aboutMeCard}>
+                        <div className={`${styles.aboutMeCard} hidden`}>
                             <div className={`${styles.aboutMeCardImage} svg-container`}><Image src={backendIcon} alt='Backend icon' /></div>
                             <h3>Backend development</h3>
                             <h4>1+ years</h4>

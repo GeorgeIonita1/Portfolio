2fee973010d1046e85c62104d99179ee73d91379
gDaddy <g3orrg301@gmail.com>
2023-08-21 22:32:21 +0300

    Change Experience styling desktop

--- client/src/components/Experience/ExperienceCard.tsx
+++ client/src/components/Experience/ExperienceCard.tsx
@@ -6,14 +6,30 @@ import { ExperiencesProps } from '@/types/interfaces';
 
 export default function ExperienceCard({ data }: ExperiencesProps) {
     return (
+        // <div className={styles.experienceCard}>
+        //         <h3>{data.title}</h3>
+        //         <h4>{data.company}</h4>
+                // <ul>
+                //     {data.description.map((item, index) => (
+                //         <li key={index}>{item}</li>
+                //     ))}
+                // </ul>
+        //     <div className={styles.experienceCardSkeleton}>
+                // <div className={`${styles.experienceCardImage} svg-container`}>
+                //     <Image src={githubIcon} alt='Github logo' />
+                // </div>
+        //     </div>
+        // </div>
         <div className={styles.experienceCard}>
-            <h3>{data.title}</h3>
-            <h4>{data.company}</h4>
-            <ul>
-                {data.description.map((item, index) => (
-                    <li key={index}>{item}</li>
-                ))}
-            </ul>
+            <div className={styles.experienceCardDetails}>
+                <h3>{data.title}</h3>
+                <h4>{data.company}</h4>
+                <ul>
+                    {data.description.map((item, index) => (
+                        <li key={index}>{item}</li>
+                    ))}
+                </ul>
+            </div>
             <div className={styles.experienceCardSkeleton}>
                 <div className={`${styles.experienceCardImage} svg-container`}>
                     <Image src={githubIcon} alt='Github logo' />
